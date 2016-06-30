c920e2d0e2e5f737695db4593c7bc794a1f93cd8
Kevin Lacker <lacker@gmail.com>
2016-06-29 18:24:44 -0700

    get all the webpacky stuff going from inside run.js

--- run.js
+++ run.js
@@ -1,3 +1,41 @@
 #!/usr/bin/env node
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 
-console.log('running');
+var config = {
+  entry: './shim.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
+      }
+    ]
+  },
+};
+
+var compiler = webpack(config);
+var options = {
+  quiet: false,
+  noInfo: false,
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false,
+  },
+};
+var server = new WebpackDevServer(compiler, options);
+
+var port = 8080;
+server.listen(port, function(error) {
+  console.log('running at http://localhost:' + port);
+});
