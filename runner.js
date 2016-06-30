6d714fa03fa5c5ddf606435259743e8a025b69b2
Kevin Lacker <lacker@gmail.com>
2016-06-29 17:31:39 -0700

    make it so that default isnt required

--- runner.js
+++ runner.js
@@ -1,7 +1,16 @@
 
 var ReactDOM = require('react-dom');
 var React = require('react');
-var Root = require('./index.js').default;
+var index = require('./index.js');
 
-ReactDOM.render(React.createElement(Root, null),
+// Figure out what the root app was exported as
+var root = null;
+for (var key in index) {
+  if (root) {
+    throw new Error('you should only export one thing');
+  }
+  root = index[key]
+}
+
+ReactDOM.render(React.createElement(root, null),
                 document.getElementById('root'))
